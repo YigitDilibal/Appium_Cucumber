ac87e3d6d92cfed12aea8dd5b2e47364b15d8d4a
YigitDilibal <archery10@outlook.com>
2025-04-11 17:09:47 +0300

    first commit

---
+++ src/test/java/runners/Runner.java
@@ -0,0 +1,24 @@
+package runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"pretty",
+                "json:target/cucumber.json",},
+        features = "src/test/resources/Features",
+        glue = "stepDefinitions",
+        tags = "@apk",
+        dryRun = false
+)
+
+
+
+public class Runner {
+
+
+
+}
